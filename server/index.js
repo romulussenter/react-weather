f71711703757d152045a9a8803aeb1d6d92e9d7c
Romulus <romulus.senter@yahoo.com>
2018-02-21 20:26:17 -0500

    made the proxy service for the server

--- server/index.js
+++ server/index.js
@@ -11,13 +11,14 @@ const serverApp =express();
 const port = process.env.PORT || 5000;
 
 serverApp.get('/forcast/:lat,:lon', function(request, response){
-    const url =`https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233`
+    const { lat, lon} = request.params;
+    const url =`https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}`
     axios.get(url)
     .then(res => {
-        response.json(res.data);
+        response.status(200).json(res.data);
     })
     .catch(err => {
-        response.json({
+        response.status(500).json({
             msg:'Your stuff is broked.'
         });
     });
